64ee481d7b324129492b7fe2e378b261cd7c3173
Eric Corson <public@giraffedev.com>
2022-08-09 07:25:14 +0100

    env: add initial flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Custom Elements in Idris";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            idris
+            gnumake
+          ];
+        };
+      }
+    );
+}
