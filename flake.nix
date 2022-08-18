5e29ee3f5ba70aba5f334f2050d671b283a734dd
Eric Corson <public@giraffedev.com>
2022-08-18 21:54:30 +0100

    env: give a slightly better flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Custom Elements in Idris";
+  description = "Custom Elements for the web in Idris";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
